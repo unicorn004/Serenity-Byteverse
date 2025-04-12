dc3a9034e3984a89ec53cc982e4641a39e50fa69
Aryan_Kanyawar <aryankan2020@gmail.com>
2025-04-12 23:04:57 +0530

    Signup works

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -2,15 +2,22 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 import { Button } from "./ui/button";
+
+import { useAuthContext } from "../context/useAuthContext";
+
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { isAuthenticated } = useAuthContext();
 
   return (
     <header className="xl:px-15 lg:px-12 px-3 sticky top-0 z-50 border-b bg-white shadow-sm">
       <div className="container mx-auto px-4">
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
-          <Link to="/" className="flex items-center gap-2 text-xl font-bold text-indigo-600">
+          <Link
+            to="/"
+            className="flex items-center gap-2 text-xl font-bold text-indigo-600"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -69,9 +76,19 @@ export default function Navbar() {
 
           <div className="flex items-center gap-4">
             {/* Sign In Button */}
-            <Button variant="default" className="hidden md:inline-flex">
-            <Link to="/login" > <p className="text-white"> Sign In </p></Link>
-          </Button>
+            {isAuthenticated ? (
+              <img src="https://cdn3.iconfinder.com/data/icons/communication-social-media-1/24/account_profile_user_contact_person_avatar_placeholder-1024.png" 
+              alt="Group Profile" 
+              className="h-10 w-10 rounded-full object-cover border border-gray-300 shadow-sm" />
+         
+            ) : (
+              <Button variant="default" className="hidden md:inline-flex">
+                <Link to="/login">
+                  {" "}
+                  <p className="text-white"> Sign In </p>
+                </Link>
+              </Button>
+            )}
 
             {/* Mobile Menu Button - Completely rebuilt */}
             <button
