0f47f7919192fdba50f4a4e72188e83c85b87242
Aroosh Joshi <dcarooshjoshi@gmail.com>
2025-04-12 23:04:57 +0530

    updated settings.py

--- django-backend/serenity/asgi.py
+++ django-backend/serenity/asgi.py
@@ -8,9 +8,26 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'serenity.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dc1.settings")
 application = get_asgi_application()
+
+
+
+### CONFIGURE SETTINGS AND GET_ASGI_PPLICATION ALWAYS BEFORE THESE INTERNAL APP IMPORTS !!
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
+
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
+
